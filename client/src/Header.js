4ce6843b8e3cd76423f29c168fffa231a6421e3b
Berta López <bertalopez@MacBook-Pro-de-Berta.local>
2023-02-19 16:56:29 +0100

    feat: conect api with mongoDB

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,31 +1,16 @@
 import {Link} from "react-router-dom";
-import {useContext, useEffect, useState} from "react";
+
 
 export default function Header() {
-  const {setUserInfo,userInfo} = useContext('');
-  useEffect(() => {
-    fetch('http://localhost:4000/profile', {
-      credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
-        setUserInfo(userInfo);
-      });
-    });
-  }, []);
-
-  const username = userInfo?.username;
 
   return (
     <header>
       <Link to="/" className="logo">MyBlog</Link>
       <nav>
-        {username}
-        {!username && (
           <>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
           </>
-        )}
       </nav>
     </header>
   );
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,32 +1,18 @@
-import {useContext, useState} from "react";
-import {Navigate} from "react-router-dom";
+import {useState} from "react";
 
 export default function LoginPage() {
   const [username,setUsername] = useState('');
   const [password,setPassword] = useState('');
-  const [redirect,setRedirect] = useState(false);
-  const {setUserInfo} = useContext('');
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
+    /* const response = await fetch('http://localhost:4000/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
       credentials: 'include',
-    });
-    if (response.ok) {
-      response.json().then(userInfo => {
-        setUserInfo(userInfo);
-        setRedirect(true);
-      });
-    } else {
-      alert('wrong credentials');
-    }
+    }); */
   }
 
-  if (redirect) {
-    return <Navigate to={'/'} />
-  }
   return (
     <form className="login" onSubmit={login}>
       <h1>Login</h1>
