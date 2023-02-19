a7b5b3edf91337ad8b836dea5db9fed5f2f203ab
Berta López <bertalopez@MacBook-Pro-de-Berta.local>
2023-02-19 15:30:13 +0100

    feat: add api user and post

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://blog:SmGDfd0ywxpTToiE@cluster0.ezhpktu.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
+    const userDoc = await User.create({
+      username,
+      password:bcrypt.hashSync(password,salt),
+    });
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+app.listen(4000);
